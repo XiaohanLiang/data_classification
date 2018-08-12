e48e8b21b989f8bffcec27af29583b5daa551972
root <Liangxiaohan95@gmail.com>
2018-08-12 18:20:10 +0100

    Now given sentence file,vector will be saved to vector_info.py

--- sentence_to_vec.py
+++ sentence_to_vec.py
@@ -11,22 +11,28 @@
 #
 
 from gensim.models import Word2Vec
+from bs4 import BeautifulSoup
 import numpy as np
-import jieba
 import re
+
 model = Word2Vec.load("./model_storage/model")
-saveDir = ""
+saveDir = "./vector_info.txt"
+file_name = "./twitter_test.txt"
+
+def tweets_to_sentences(data):
+    data_text = BeautifulSoup(data).get_text()
+    sentences = re.sub('[^a-zA-Z]', ' ', data_text.lower().strip()).split()
+    return sentences
 
 def split_text_file(file_name):
     raw_data = []
-    w = open(filename,'r',encoding='utf-8')
+    w = open(file_name,'r')
     for line in w.readlines():
-        newline = line.strip()
-        newline = re.sub(' ','',newline)
-        newline = jieba.cut(newline)
-        raw_data.append(list(newline))
+        newline = tweets_to_sentences(line)
+        raw_data.append(newline)
     w.close()
     return raw_data
+
 
 #
 # @Function: From this function when we input a sentence 
@@ -35,15 +41,22 @@ def split_text_file(file_name):
 #
 #
 
-def sentence_to_vec(sentence):
-    sentence_vector = []
-    for word in sentence:
-        try:
-            sentence_vector.append(model.wv[word])
-        except:
-            continue
-    sentence_vector = np.array(sentence_vector)
-    v = sentence_vector.sum(axis = 0)
-    return v/np.sqrt((v ** 2).sum())
+def sentence_to_vec(sentence_array):
+    return_ans = []
+    for sentence in sentence_array:
+        sentence_vector = []
+        for word in sentence:
+            try:
+                sentence_vector.append(model.wv[word])
+            except:
+                continue
+        sentence_vector = np.array(sentence_vector)
+        v = sentence_vector.sum(axis = 0)
+        ans =  v/np.sqrt((v ** 2).sum())
+        return_ans.append(ans)
+    return return_ans
 
-
+if(__name__=='__main__'):
+    raw_data = split_text_file(file_name)
+    sentence_vec = sentence_to_vec(raw_data)
+    np.savetxt(saveDir,sentence_vec,delimiter=',')
