1639afad105f852cff0cd228c5edbca8d5c227ec
root <Liangxiaohan95@gmail.com>
2018-08-15 00:08:04 +0100

    1234

--- sentence_to_vec_new.py
+++ sentence_to_vec_new.py
@@ -1,3 +1,4 @@
+
 #
 # @Function:This script is designed to trnasform sentences to a 
 #           100 dimensional vector
@@ -10,7 +11,7 @@
 #
 
 from gensim.models import Word2Vec
-from bs4 import BeautifulSoup
+import keyword_parser
 import numpy as np
 import re
 import sys
@@ -20,30 +21,34 @@ params = eval(open("settings.txt").read())
 model = Word2Vec.load(params['model_path'])
 file_name = params['twitter_path']
 
-def tweets_to_sentences(data):
-    data_text = BeautifulSoup(data).get_text()
-    sentences = re.sub('[^a-zA-Z]', ' ', data_text.lower().strip()).split()
-    return sentences
+#
+# @Function:This function was used to break into word
+#
 
-def split_text_file(file_name):
+def break_into_word(phrase_array):
+    word_array = []
+    for phrase in phrase_array: 
+        phrase_to_word = re.sub('[^a-zA-Z]', ' ', phrase.lower().strip()).split()
+        word_array = word_array + phrase_to_word
+    return word_array
+
+def generate_words_array(file_name):
     raw_data = []
     w = open(file_name,'r')
     for line in w.readlines():
-        newline = tweets_to_sentences(line)
+        newline = keyword_parser.get_keywords(line, ['NP'])
+        newline = break_into_word(newline)
         raw_data.append(newline)
     w.close()
     return raw_data
-
-
 #
 # @Function: From this function when we input a sentence 
 #            in the word list form we would get a vector 
 #            back which is the vector of this sentence
 #
-#
 
 def sentence_to_vec():
-    sentence_array = split_text_file(file_name)
+    sentence_array = generate_words_array(file_name)
     return_ans = []
     for sentence in sentence_array:
         sentence_vector = []
@@ -58,3 +63,4 @@ def sentence_to_vec():
         return_ans.append(ans)
     return return_ans
 
+sentence_to_vec()
